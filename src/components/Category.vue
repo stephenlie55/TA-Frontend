2e20d6689f24e6c1f858a66aec2b554b62d644b6
stephenlie55 <stephenlie55@yahoo.com>
2019-11-03 17:11:04 +0700

    Add filter and sorting products by its total sales

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container" style="margin: 20px; margin-left: 30px; background: white; width: 80%; border-radius: 5px; max-height: 500px; display: flex; overflow-y: scroll">
         <ul class="list-group">
-            <EachCategory v-for="(category, index) in categories" :key="index" :category="category"/>
+            <EachCategory v-for="(category, index) in categories" :key="index" :category="category.data[0].productName"/>
         </ul>
   </div>
 </template>
@@ -15,19 +15,38 @@
         data: () => ({
             categories: []
         }),
-        created() {
-            for (var key in this.$store.state.products) {
-                this.categories.push(key)
+        methods: {
+            sortingData() {
+                var swapped;
+                do {
+                    swapped = false;
+                    for (var i=0; i < this.categories.length-1; i++) {
+                        if (this.categories[i].data.length < this.categories[i+1].data.length) {
+                            var temp = this.categories[i].data.length;
+                            this.categories[i].data.length = this.categories[i+1].data.length;
+                            this.categories[i+1].data.length = temp;
+                            swapped = true;
+                        }
+                    }
+                } while (swapped);
             }
+        },
+        created() {
+            this.categories = []
+            for (var key in this.$store.state.products) {
+                this.categories.push(this.$store.state.products[key])
+            }
+            this.sortingData()
         },
         watch: {
             '$store.state.search'() {
                 this.categories = []
                 for (var key in this.$store.state.products) {
                     if (key.toLowerCase().search(this.$store.state.search.toLowerCase()) !== -1) {
-                        this.categories.push(key)
+                        this.categories.push(this.$store.state.products[key])
                     }
                 }
+                this.sortingData()
             }
         }
     }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,16 +3,12 @@
     <NavBar v-if="$store.state.flag"/>
     <div class="container" style="background: #E4E5E6; height: 100%; width: 100%; padding: 0px; margin: 0 auto;justify-content: space-between">
       <div class="row" style="justify-content: space-between">
-        <div class="col-3" style="margin: 0px; padding: 0px;">
-          <div v-if="$store.state.flag">
-            <Category/>
-          </div>
-        </div>
-        <div class="col-9" style="margin: 0px; padding: 0px;right: 15px;">
-          <div v-if="$store.state.loaded">
-            <ChartContainer/>
-          </div>
+        <div class="col-9" style="margin: 0px; padding: 0px;">
+          <ChartContainer v-if="$store.state.loaded" />
         </div>  
+        <div class="col-3" style="margin: 0px; padding: 0px;" v-if="$store.state.flag">
+          <Category/>
+        </div>
       </div>
     </div>
   </div>
