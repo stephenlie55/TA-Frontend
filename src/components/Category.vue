bd01982331eea5500aef2276c16351738dfd4fc9
stephenlie55 <stephenlie55@yahoo.com>
2019-11-03 18:06:21 +0700

    bug fixed in sorting

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container" style="margin: 20px; margin-left: 30px; background: white; width: 80%; border-radius: 5px; max-height: 500px; display: flex; overflow-y: scroll">
         <ul class="list-group">
-            <EachCategory v-for="(category, index) in categories" :key="index" :category="category.data[0].productName"/>
+            <EachCategory v-for="(category, index) in categories" :key="index" :category="category"/>
         </ul>
   </div>
 </template>
@@ -16,15 +16,15 @@
             categories: []
         }),
         methods: {
-            sortingData() {
+            sorting() {
                 var swapped;
                 do {
                     swapped = false;
                     for (var i=0; i < this.categories.length-1; i++) {
-                        if (this.categories[i].data.length < this.categories[i+1].data.length) {
-                            var temp = this.categories[i].data.length;
-                            this.categories[i].data.length = this.categories[i+1].data.length;
-                            this.categories[i+1].data.length = temp;
+                        if (this.$store.state.products[this.categories[i]].data.length < this.$store.state.products[this.categories[i+1]].data.length) {
+                            var temp = this.categories[i]
+                            this.categories[i] = this.categories[i+1]
+                            this.categories[i+1] = temp;
                             swapped = true;
                         }
                     }
@@ -32,21 +32,20 @@
             }
         },
         created() {
-            this.categories = []
             for (var key in this.$store.state.products) {
-                this.categories.push(this.$store.state.products[key])
+                this.categories.push(key)
             }
-            this.sortingData()
+            this.sorting()
         },
         watch: {
             '$store.state.search'() {
                 this.categories = []
                 for (var key in this.$store.state.products) {
                     if (key.toLowerCase().search(this.$store.state.search.toLowerCase()) !== -1) {
-                        this.categories.push(this.$store.state.products[key])
+                        this.categories.push(key)
                     }
                 }
-                this.sortingData()
+                this.sorting()
             }
         }
     }
